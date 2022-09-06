b3b1d9a1622fd7413281b8d26753ac879ab4369a
Marwan Tarik <96257489+MarwanTarik@users.noreply.github.com>
2022-09-06 17:59:57 +0200

    Add files via upload

--- sudoko.pl
+++ sudoko.pl
@@ -6,13 +6,18 @@ sudoko(Rowes) :-
     maplist(same_length(Rowes), Rowes),
     append(Rowes, Board),
     Board ins 1..9,
+    
     maplist(all_distinct, Rowes),
     transpose(Rowes, Columns),
     maplist(all_distinct, Columns),
+
     Rowes = [As,Bs,Cs,Ds,Es,Fs,Gs,Hs,Is],
     squares(As, Bs, Cs),
     squares(Ds, Es, Fs),
-    squares(Gs, Hs, Is).
+    squares(Gs, Hs, Is),
+
+    maplist(label, Rowes),
+    maplist(portray_clause, Rowes).
 
     squares([], [], []).
     squares([E1,E2,E3|Es1],
@@ -20,6 +25,8 @@ sudoko(Rowes) :-
             [E7,E8,E9|Es3]) :-
              all_distinct([E1,E2,E3,E4,E5,E6,E7,E8,E9]),
              squares(Es1, Es2, Es3).
+    
+   
 
 
 
